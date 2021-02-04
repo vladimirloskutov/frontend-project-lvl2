f42241a35abe31993d9a8e3bbdaae26c37fd097d
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-02-04 22:10:50 +0300

    add version and description for --help information

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+
+import { program } from 'commander';
+
+// Здесь сразу вопрос по поводу статьи скрипты/модули, стоит ли код хэлпа размещать здесь?
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .parse();
