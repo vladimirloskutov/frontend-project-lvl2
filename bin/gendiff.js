14d526f1fa2b1bc5fdc3ac2312207773fbb78a6d
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-02-08 21:09:04 +0300

    some fixes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,7 @@ import { program } from 'commander';
 
 // Вопрос по поводу статьи скрипты/модули, стоит ли код хэлпа размещать здесь?
 program
-  .version('0.0.1')
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
