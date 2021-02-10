adf1a69431326790659ce8bbbe0f657364ccd97b
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-02-11 01:08:22 +0300

    import gendiff function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
-// Вопрос по поводу статьи скрипты/модули, стоит ли код хэлпа размещать здесь?
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
   .parse();
